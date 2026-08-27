32cbffade7014167bce2f8c31d12035aef9e7e99
DSagnik24 <sagnikduttaofficial18@gmail.com>
2026-08-27 22:44:05 +0530

    Implementing Backend and Database

---
+++ forgeFit_Backend/src/main/java/com/forgefit/forgeFit_Backend/ForgeFitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.forgefit.forgeFit_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgeFitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgeFitBackendApplication.class, args);
+	}
+
+}
---
+++ forgeFit_Backend/src/test/java/com/forgefit/forgeFit_Backend/ForgeFitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forgefit.forgeFit_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgeFitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
